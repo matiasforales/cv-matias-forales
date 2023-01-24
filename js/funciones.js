9d37390c2647d57175d5066fd4f941ea3e8abf03
matias forales <matiasforales@live.com.ar>
2023-01-24 11:22:03 -0300

    24/01/2023

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,17 @@
 document.getElementById("boton").addEventListener("click",function(){
-    console.log("hola mundo desde EventListener");
     document.getElementById("texto aclaratorio").innerHTML = "Por favor, se tolerante con los errores. ;)"
+})
+
+document.getElementById("boton_volver").addEventListener("click",function(){
+    document.getElementById("texto aclaratorio").innerHTML = "Hola, estoy aprendiendo JavaScript."
+})
+
+document.getElementById("boton_ocultar").addEventListener("click",function(){
+    document.getElementById("texto aclaratorio").style.display = "none";
+})
+
+document.getElementById("boton_revelar").addEventListener("click",function(){
+    document.getElementById("texto aclaratorio").style.display = "block";
 })
 
 document.getElementById("boton_color").addEventListener("click",function(){
@@ -11,10 +22,6 @@ document.getElementById("boton_default").addEventListener("click",function(){
     document.body.style.backgroundColor = "rgb(252, 247, 241)";
 })
 
-document.getElementById("boton_ocultar").addEventListener("click",function(){
-    document.getElementById("texto aclaratorio").style.display = "none";
-})
-
-document.getElementById("boton_revelar").addEventListener("click",function(){
-    document.getElementById("texto aclaratorio").style.display = "block";
+document.getElementById("boton_fecha").addEventListener("click",function(){
+    document.getElementById("fecha_en_vivo").innerHTML=Date()
 })
